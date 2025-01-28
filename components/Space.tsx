69b20cfac101191f8a59aa610226520b924bf848
Skylar Beaty <31560421+skylarbeaty@users.noreply.github.com>
2025-01-28 13:42:05 -0500

    Tiles animate smoothly between spaces
    Tiles can be drag and dropped between spaces. When dragging a tile the potential spaces are outlined green. When the left mouse button is let go the tile gets its new space and animates to it. Also the tiles now gracefully load in, snapping to their initial positions and don't render until they are ready to do so.

--- components/Space.tsx
+++ components/Space.tsx
@@ -1,4 +1,4 @@
-import { useRef, useLayoutEffect } from 'react'
+import { useRef, useLayoutEffect, useState } from 'react'
 import { useGameContext } from './Game';
 
 interface SpaceProps {
@@ -7,16 +7,40 @@ interface SpaceProps {
 
 const Space = ({id}: SpaceProps) => {
   const gameContext = useGameContext()!
-  const divRef =  useRef<HTMLDivElement | null>(null);
+  const divRef =  useRef<HTMLDivElement | null>(null)
+  const [mouseHover, setMouseHover] = useState<boolean>(false)
 
   useLayoutEffect(() => {
     gameContext.updateSpace(id, divRef.current)
   })
 
+  const handleMouseEnter = (e: React.MouseEvent) => {
+    if (gameContext.dragID != -1)
+      setMouseHover(true)
+  }
+
+  const handleMouseLeave = (e: React.MouseEvent) => {
+    setMouseHover(false)
+  }
+
+  const handleMouseUp = (e: React.MouseEvent) => {
+    if (gameContext.dragID != -1){
+      gameContext.moveTile(gameContext.dragID, id)
+      setMouseHover(false)
+    }
+  }
+
   return (
-    <div className='border-indigo-400 border border-dashed rounded-md w-10 h-10'
-      ref={divRef}
-    />
+    <>
+      <div className={`w-10 h-10 overflow-visible rounded-md
+        ${mouseHover ? `ring-2 ring-emerald-700 border-emerald-400 border-solid` : 
+          `border-indigo-400 border border-dashed`}`}
+        ref={divRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onMouseUp={handleMouseUp}
+      />
+    </>
   )
 }
 
--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,5 +1,7 @@
-import { useRef, forwardRef, useLayoutEffect, useEffect } from 'react'
+import { useRef, forwardRef, useLayoutEffect, useEffect, useState } from 'react'
 import { useGameContext } from './Game';
+import { start } from 'repl';
+import { read } from 'fs';
 
 interface TileProps {
   id: number
@@ -7,9 +9,13 @@ interface TileProps {
 }
 
 const Tile = forwardRef<HTMLDivElement, TileProps>((props, ref) => {
-  const targetPos = useRef<{ left: number; top: number } | null>(null)
-  const tileGhostRef = useRef<HTMLDivElement | null>(null)
   const gameContext = useGameContext()!
+  const targetPos = useRef<{ left: number; top: number } | null>(null)
+  const prevPos = useRef<{left: number, top: number} | null>(null)
+  const initPos = useRef<{left: number, top: number} | null>(null)
+  const tileGhostRef = useRef<HTMLDivElement | null>(null)
+  const [ready, setReady] = useState<boolean>(false)
+  const [ghostReady, setGhostReady] = useState<boolean>(false)
   const myTile = gameContext.tiles[props.id - 1]
   const mySpace = gameContext.spaces[myTile.spaceID - 1]
   let mouseDelta = {x: 0, y: 0}
@@ -21,24 +27,36 @@ const Tile = forwardRef<HTMLDivElement, TileProps>((props, ref) => {
       targetPos.current = {left: rect.left, top: rect.top}
     }
 
-    // animate to target position
-    if (targetPos.current && myTile.divRef){
+    // setup position first time rendering
+    if (!prevPos.current && targetPos.current && myTile.divRef){
       const rect = myTile.divRef.getBoundingClientRect()
-      const deltaX = targetPos.current.left - rect.left
-      const deltaY = targetPos.current.top - rect.top
+      prevPos.current = {...targetPos.current}
+      initPos.current = {left: rect.left, top: rect.top}
+    }
+
+    // animate to target position
+    if (targetPos.current && myTile.divRef && prevPos.current && initPos.current){
+      const rect = myTile.divRef.getBoundingClientRect()
+      const startX = prevPos.current.left - initPos.current.left
+      const startY = prevPos.current.top - initPos.current.top
+      const deltaX = targetPos.current.left - prevPos.current.left
+      const deltaY = targetPos.current.top - prevPos.current.top
+
+      myTile.divRef.style.transform = `translate(${startX}px, ${startY}px)`
+      myTile.divRef.style.transition = "transform"
 
       if (deltaX || deltaY){
-        myTile.divRef.style.transform = ""
-        myTile.divRef.style.transition = "transform"
-
         requestAnimationFrame(() => {
           if (myTile.divRef) {
-            myTile.divRef.style.transform = `translate(${deltaX}px, ${deltaY}px)`
+            myTile.divRef.style.transform = `translate(${startX + deltaX}px, ${startY + deltaY}px)`
             myTile.divRef.style.transition = "transform 300ms ease-in-out"
+            if (prevPos.current)
+              prevPos.current = {...targetPos.current!}
           }
         })
       }
     }
+    setReady(true)
   }, [myTile?.spaceID, gameContext.spaces])
 
   const handleMouseDown = (e: React.MouseEvent) => {
@@ -46,7 +64,6 @@ const Tile = forwardRef<HTMLDivElement, TileProps>((props, ref) => {
 
     const rect = myTile.divRef?.getBoundingClientRect()!
     mouseDelta = {x: e.clientX - rect.x, y: e.clientY - rect.y}
-    console.log(mouseDelta.x)
     updateGhost(e.clientX - mouseDelta.x, e.clientY - mouseDelta.y)
     gameContext.changeDragID(props.id)
 
@@ -56,12 +73,14 @@ const Tile = forwardRef<HTMLDivElement, TileProps>((props, ref) => {
 
   const handleMouseMove = (e: MouseEvent) => {
     e.preventDefault() 
+    setGhostReady(true);
     updateGhost(e.clientX - mouseDelta.x, e.clientY - mouseDelta.y)
   }
 
   const handleMouseUp = (e: MouseEvent) => {
     e.preventDefault() 
     gameContext.changeDragID(-1)
+    setGhostReady(true);
     document.removeEventListener("mousemove", handleMouseMove)
     document.removeEventListener("mouseup", handleMouseUp)
   }
@@ -75,20 +94,22 @@ const Tile = forwardRef<HTMLDivElement, TileProps>((props, ref) => {
 
   return (
     <>
-      <div className={`w-10 h-10 flex fixed items-center justify-center rounded bg-indigo-600 text-white drop-shadow-md
+      <div className={`w-10 h-10 flex fixed items-center justify-center rounded 
+        ${ready && "bg-indigo-600 text-white drop-shadow-md"}
         ${gameContext.dragID === myTile.id && "animate-pulse"}
         ${gameContext.dragID === -1 ? "draggable" : "dragging"}
         `}
         ref={ref}
         onMouseDown={handleMouseDown}
       >
-        {props.letter}
+        {ready && props.letter}
       </div>
       {gameContext.dragID === myTile.id &&
-        <div className='dragging w-10 h-10 flex fixed items-center justify-center rounded bg-indigo-400 animate-pulse text-white drop-shadow-md'
+        <div className={`dragging pointer-events-none w-10 h-10 flex fixed items-center justify-center rounded 
+          ${ghostReady && 'bg-indigo-400 animate-pulse text-white drop-shadow-md'}`}
           ref={tileGhostRef}
         >
-          {props.letter}
+          {ghostReady && props.letter}
         </div>
       }
     </>
