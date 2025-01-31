72aac423bcc58e948f03c1fba0f962844dfc4ab7
Skylar Beaty <31560421+skylarbeaty@users.noreply.github.com>
2025-01-31 04:49:32 -0500

    Restart and shuffle buttons working
    Restarting moves all the tiles back to the their original positions (from the board and panel). Shuffle sends all the panel tiles to a random panel space

--- components/ButtonPanel.tsx
+++ components/ButtonPanel.tsx
@@ -1,35 +1,59 @@
 import React from 'react'
 import { useGameContext } from './Game'
+import { shuffleArray } from '@/util/shuffle'
 
 const ButtonPanel = () => {
   const gameContext = useGameContext()!
   const tiles = gameContext.tiles
   const spaces = gameContext.spaces
   const solution = gameContext.solution
+  const moveTile = gameContext.moveTile
 
-  const handleReturnButton = () => {
+  const handleReturn = () => {
     const boardTiles = tiles.filter(tile => spaces.find(space => space.id === tile.spaceID)?.position.container === "board")
     const returnTiles = boardTiles.filter(tile => !solution.solutionTiles.has(tile.id))
-    let emptySpaces = spaces.filter(space => space.position.container === "panel" 
-                                    && !tiles.find(tile => tile.spaceID === space.id))
+    let emptySpaces = spaces.filter(space => space.position.container === "panel" && !tiles.find(tile => tile.spaceID === space.id))
 
     returnTiles.forEach(tile => {
       if (emptySpaces.length > 0)
-        gameContext.moveTile(tile.id, emptySpaces.shift()!.id)
+        moveTile(tile.id, emptySpaces.shift()!.id) // move each tile to the next empty space
       else
         console.log("Error: No spaces left to return tiles")
+    })
+  }
+
+  const handleRestart = () => {
+    tiles.forEach(tile => {
+      moveTile(tile.id, tile.id + 140) // move each tile to its original location
+    })
+  }
+
+  const handleShuffle = () => {
+    const panelTiles = tiles.filter(tile => spaces.find(space => space.id === tile.spaceID)?.position.container === "panel")
+    let unusedSpaces = shuffleArray(spaces.filter(space => space.position.container === "panel"))
+
+    panelTiles.forEach(tile => {
+      moveTile(tile.id, unusedSpaces.shift()!.id)
     })
   }
 
   return (
     <div className='bg-indigo-200 mt-2 mb-2 p-4 rounded shadow-lg flex justify-between'>
         <button className='bg-indigo-800 text-white p-3 ml-1 min-w-24 rounded-lg'
-          onPointerDown={handleReturnButton}
+          onPointerDown={handleReturn}
         >
           Return
         </button>
-        <button className='bg-indigo-800 text-white p-3 mr-1 mr-1 min-w-24 rounded-lg'>Restart</button>
-        <button className='bg-indigo-800 text-white p-3 ml-1 mr-1 min-w-24 rounded-lg'>Shuffle</button>
+        <button className='bg-indigo-800 text-white p-3 mr-1 mr-1 min-w-24 rounded-lg'
+          onPointerDown={handleRestart}
+        >
+          Restart
+        </button>
+        <button className='bg-indigo-800 text-white p-3 ml-1 mr-1 min-w-24 rounded-lg'
+          onPointerDown={handleShuffle}
+        >
+          Shuffle
+        </button>
         <button className='bg-indigo-800 text-white p-3 ml-1 min-w-24 rounded-lg'>Move</button>
     </div>
   )
