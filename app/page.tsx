e6ee0e5f25a703eabc7c634f520d8093330fa775
Skylar Beaty <31560421+skylarbeaty@users.noreply.github.com>
2025-02-06 13:39:50 -0500

    Centering the game page
    Centering the element that hold the game, and setting it to fill up the whole available window

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Game from '@/components/Game';
 
 export default function Home() {
   return (
-    <div className="bg-slate-100 flex items-center justify-center">
-      <div className="h-[100dvh] max-w-lg w-full">
+    <div className="bg-slate-100 flex items-center justify-center justify-self-center w-full">
+      <div className="h-[window.innerHeight] h-[window.innerHeight]">
         <Game />        
       </div>
     </div>
