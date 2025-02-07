533eeb60f9ff78dfa0720b6d71aad3ae415425a5
Skylar Beaty <31560421+skylarbeaty@users.noreply.github.com>
2025-02-07 03:39:21 -0500

    Added hover and active to buttons
    Added styling for hovering or pressing a button

--- components/GameButton.tsx
+++ components/GameButton.tsx
@@ -9,7 +9,7 @@ interface GameButtonProps {
 const GameButton = ({ children, handlePointerDown = () => {}, disabled = false }: GameButtonProps) => {
   return (
     <button disabled={disabled} className={`bg-indigo-800 text-white
-        disabled:bg-slate-400
+        disabled:bg-slate-400 active:bg-indigo-500 hover:bg-indigo-700
         size-4 xs-box:size-6 sm-box:size-9 md-box:size-12
         rounded-md sm-box:rounded-lg
         p-[2px]   xs-box:p-[4px]    sm-box:p-[6px]    md-box:p-[8px]
