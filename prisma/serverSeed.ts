e97354bf9f22f44571f69c401e02e3af98718d46
Skylar Beaty <nebulasvoid93@gmail.com>
2025-09-12 22:23:16 -0400

    Updated file imports in server seed
    Changed the syntax for importing fs and path for use on the server

--- prisma/serverSeed.ts
+++ prisma/serverSeed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
-import fs from 'node:fs'
-import path from 'node:path'
+import * as fs from 'fs'
+import * as path from 'path'
 
 const prisma = new PrismaClient()
 
